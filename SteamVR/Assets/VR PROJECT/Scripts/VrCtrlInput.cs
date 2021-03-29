a6f8ded1bc4e4cfbdf47615c58b3bcb740d58ed1
Lala-banners <lala.l.banno@hotmail.com>
2021-03-29 12:34:28 +1100

    Changed Use to Interact

--- SteamVR/Assets/VR PROJECT/Scripts/VrCtrlInput.cs
+++ SteamVR/Assets/VR PROJECT/Scripts/VrCtrlInput.cs
@@ -12,8 +12,8 @@ namespace BreadAndButterVR
         public VRInputEvent OnPointerReleased => onPointerReleased;
         public VRInputEvent OnTeleportPressed => onTeleportPressed;
         public VRInputEvent OnTeleportReleased => onTeleportReleased;
-        public VRInputEvent OnUsePressed => onUsePressed;
-        public VRInputEvent OnUseReleased => onUseReleased;
+        public VRInputEvent OnInteractPressed => onInteractPressed;
+        public VRInputEvent OnInteractReleased => onInteractReleased;
         public VRInputEvent OnGrabPressed => onGrabPressed;
         public VRInputEvent OnGrabReleased => onGrabReleased;
         public VRInputEvent OnTouchPadAxisChanged => onTouchPadChanged;
@@ -23,7 +23,7 @@ namespace BreadAndButterVR
         [Header("Steam Actions")]
         [SerializeField, Tooltip("Pointer action in VR")] private SteamVR_Action_Boolean pointer;
         [SerializeField, Tooltip("Teleport action in VR")] private SteamVR_Action_Boolean teleport;
-        [SerializeField, Tooltip("Use action in VR")] private SteamVR_Action_Boolean use;
+        [SerializeField, Tooltip("Use action in VR")] private SteamVR_Action_Boolean interact;
         [SerializeField, Tooltip("Grab action in VR")] private SteamVR_Action_Boolean grab;
         [SerializeField, Tooltip("Touch pad in VR")] private SteamVR_Action_Vector2 touchPadAxis;
         #endregion
@@ -34,8 +34,8 @@ namespace BreadAndButterVR
         [SerializeField] private VRInputEvent onPointerReleased = new VRInputEvent();
         [SerializeField] private VRInputEvent onTeleportPressed = new VRInputEvent();
         [SerializeField] private VRInputEvent onTeleportReleased = new VRInputEvent();
-        [SerializeField] private VRInputEvent onUsePressed = new VRInputEvent();
-        [SerializeField] private VRInputEvent onUseReleased = new VRInputEvent();
+        [SerializeField] private VRInputEvent onInteractPressed = new VRInputEvent();
+        [SerializeField] private VRInputEvent onInteractReleased = new VRInputEvent();
         [SerializeField] private VRInputEvent onGrabPressed = new VRInputEvent();
         [SerializeField] private VRInputEvent onGrabReleased = new VRInputEvent();
         [SerializeField] private VRInputEvent onTouchPadChanged = new VRInputEvent();
@@ -46,8 +46,8 @@ namespace BreadAndButterVR
         private void OnPointerUp(SteamVR_Action_Boolean _action, SteamVR_Input_Sources _source) => onPointerReleased.Invoke(GenerateArgs()); 
         private void OnTeleportDown(SteamVR_Action_Boolean _action, SteamVR_Input_Sources _source) => onTeleportPressed.Invoke(GenerateArgs()); 
         private void OnTeleportUp(SteamVR_Action_Boolean _action, SteamVR_Input_Sources _source) => onTeleportReleased.Invoke(GenerateArgs()); 
-        private void OnUseDown(SteamVR_Action_Boolean _action, SteamVR_Input_Sources _source) => onUsePressed.Invoke(GenerateArgs()); 
-        private void OnUseUp(SteamVR_Action_Boolean _action, SteamVR_Input_Sources _source) => onUseReleased.Invoke(GenerateArgs()); 
+        private void OnInteractDown(SteamVR_Action_Boolean _action, SteamVR_Input_Sources _source) => onInteractPressed.Invoke(GenerateArgs()); 
+        private void OnInteractUp(SteamVR_Action_Boolean _action, SteamVR_Input_Sources _source) => onInteractReleased.Invoke(GenerateArgs()); 
         private void OnGrabDown(SteamVR_Action_Boolean _action, SteamVR_Input_Sources _source) => onGrabPressed.Invoke(GenerateArgs()); 
         private void OnGrabUp(SteamVR_Action_Boolean _action, SteamVR_Input_Sources _source) => onGrabReleased.Invoke(GenerateArgs());
         private void OnTouchPadChanged(SteamVR_Action_Vector2 _action, SteamVR_Input_Sources _source, Vector2 _axis, Vector2 _delta) 
@@ -67,8 +67,8 @@ namespace BreadAndButterVR
             teleport.AddOnStateDownListener(OnTeleportDown, controller.InputSource); 
             teleport.AddOnStateUpListener(OnTeleportUp, controller.InputSource); 
 
-            use.AddOnStateDownListener(OnUseDown, controller.InputSource); 
-            use.AddOnStateUpListener(OnUseUp, controller.InputSource); 
+            interact.AddOnStateDownListener(OnInteractDown, controller.InputSource); 
+            interact.AddOnStateUpListener(OnInteractUp, controller.InputSource); 
 
             grab.AddOnStateDownListener(OnGrabDown, controller.InputSource); 
             grab.AddOnStateUpListener(OnGrabUp, controller.InputSource); 

06ff914e1876eff76b36454a8a855b57fb4abdf2
Greengreener <>
2021-05-03 14:25:25 +1000

    Moved stuff into folders

---
+++ SteamVR/Assets/Alley Oop/Scripts/Mouse/MouseThrow.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AlleyOop.VR.Interaction
+{
+    public class MouseThrow : MonoBehaviour
+    {
+        public GameObject basketball;
+
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        // OnMouseDown is called when the user has pressed the mouse button while over the GUIElement or Collider
+        private void OnMouseDown()
+        {
+
+        }
+
+        // OnMouseUp is called when the user has released the mouse button
+        private void OnMouseUp()
+        {
+
+        }
+    }
+}
